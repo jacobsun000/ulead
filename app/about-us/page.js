df87d098749644a5d3b22ae7e5b436e1966af471
Jacob Sun <jacobsun000@gmail.com>
2024-09-09 15:17:19 -0500

    feat: Placeholder for about-us page

---
+++ app/about-us/page.js
@@ -0,0 +1,10 @@
+import Header from "@/ui/Header";
+
+export default function AboutUs() {
+  return (
+    <div>
+      <Header currentPath="/about-us" />
+      <h1 className="text-4xl font-bold text-center bg-[#ccc] text-red-900">Placeholder</h1>
+    </div>
+  );
+}
