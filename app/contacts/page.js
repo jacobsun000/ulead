28db17de47d62b08d348aad3130f25d0293e14d7
York <gpyooork@gmail.com>
2024-09-09 18:44:03 -0500

    Test commit user

--- app/contacts/page.js
+++ app/contacts/page.js
@@ -21,10 +21,10 @@ export default function Contacts() {
       <Header currentPath="/contacts-me" />
       <div className="max-w-4xl mx-auto p-4">
         <div className="grid grid-cols-4 gap-4 mb-8">
-          <div className="col-span-1 bg-gray-200 h-40"></div>
-          <div className="col-span-1 bg-gray-200 h-40"></div>
-          <div className="col-span-1 bg-gray-200 h-40"></div>
-          <div className="col-span-1 bg-gray-200 h-40"></div>
+          <div className="col-span-1 bg-gray-100 h-40"></div>
+          <div className="col-span-1 bg-gray-100 h-40"></div>
+          <div className="col-span-1 bg-gray-100 h-40"></div>
+          <div className="col-span-1 bg-gray-100 h-40"></div>
         </div>
       </div>
 
