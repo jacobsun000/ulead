89a7792cb545cfa6e13c2eb087fa1c07b2207c21
York <gpyooork@gmail.com>
2024-09-15 01:42:08 -0500

    Add img for Contacts Page

--- app/contacts/page.js
+++ app/contacts/page.js
@@ -1,5 +1,6 @@
 import Header from "@/ui/Header";
 import Footer from "@/ui/Footer";
+import BackButton from "@/ui/Back";
 import Image from 'next/image'
 
 const ContactItem = ({ src, alt, text }) => (
@@ -101,7 +102,7 @@ export default function Contacts() {
         <h2 className="text-2xl font-bold text-center mb-6">Fill this form and we will get back to you ASAP</h2>
         <form className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <div>
-            <label htmlFor="name" className="block mb-1 font-medium">Your Name*</label>
+            <label htmlFor="name" className="block mb-1 font-bold">Your Name*</label>
             <input
               type="text"
               id="name"
@@ -110,7 +111,7 @@ export default function Contacts() {
             />
           </div>
           <div>
-            <label htmlFor="contact" className="block mb-1 font-medium">Email/Mobile/Wechat*</label>
+            <label htmlFor="contact" className="block mb-1 font-bold">Email/Mobile/Wechat*</label>
             <input
               type="text"
               id="contact"
@@ -119,7 +120,7 @@ export default function Contacts() {
             />
           </div>
           <div>
-            <label htmlFor="source" className="block mb-1 font-medium">How you get to know us</label>
+            <label htmlFor="source" className="block mb-1 font-bold">How you get to know us</label>
             <input
               type="text"
               id="source"
@@ -128,7 +129,7 @@ export default function Contacts() {
             />
           </div>
           <div>
-            <label htmlFor="questions" className="block mb-1 font-medium">Your concerns and questions</label>
+            <label htmlFor="questions" className="block mb-1 font-bold">Your concerns and questions</label>
             <input
               type="text"
               id="questions"
@@ -136,18 +137,15 @@ export default function Contacts() {
               className="w-full p-2 border border-gray-500 rounded"
             />
           </div>
-          <div className="md:col-span-2 mt-4 flex justify-center">
+          <div className="md:col-span-2 mt-4 mb-4 flex justify-center">
             <button type="button" className="w-1/3 py-2 px-4 bg-red-600 text-white text-xl font-semibold rounded hover:bg-red-700 transition duration-300">
               Submit
             </button>
           </div>
         </form>
-        <div className="mt-4 flex justify-center">
-          <button type="button" className="px-4 py-2 bg-gray-300 text-gray-700 font-semibold rounded hover:bg-gray-400 transition duration-300">
-            Back to top
-          </button>
-        </div>
       </div>
+
+      <BackButton />
       <Footer />
     </div>
   );
