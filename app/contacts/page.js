2b7c5d8944299fa388b03f2f3ba196f10c0684d8
York <gpyooork@gmail.com>
2024-10-24 16:59:38 -0500

    Fix: contact page style in mobile

--- app/contacts/page.js
+++ app/contacts/page.js
@@ -22,8 +22,8 @@ export default function Contacts() {
   return (
     <div>
       <Header currentPath="/contacts" />
-      <div className="md:max-w-6xl mx-auto p-4 mb-4">
-        <SectionHeader title="Our Offices" style='mt-0 mb-8 md:my-8' />
+      <div className="md:max-w-6xl mx-auto p-4 mb-4 hidden md:block">
+        <h2 className="text-xl md:text-3xl font-bold text-center mb-6">Welcome to visit our offices</h2>
         <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
           <div className="col-span-1 w-40 h-40 md:w-60 md:h-40 text-center">
             <Image
@@ -105,7 +105,7 @@ export default function Contacts() {
         <h2 className="text-2xl font-bold text-center mb-6">Fill this form and we will get back to you ASAP</h2>
         <form className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <div>
-            <label htmlFor="name" className="block mb-1 font-bold">Your Name*</label>
+            <label htmlFor="name" className="block mb-1 font-bold text-base md:text-lg">Your Name*</label>
             <input
               type="text"
               id="name"
@@ -114,7 +114,7 @@ export default function Contacts() {
             />
           </div>
           <div>
-            <label htmlFor="contact" className="block mb-1 font-bold">Email/Mobile/Wechat*</label>
+            <label htmlFor="contact" className="block mb-1 font-bold text-base md:text-lg">Email/Mobile/Wechat*</label>
             <input
               type="text"
               id="contact"
@@ -123,7 +123,7 @@ export default function Contacts() {
             />
           </div>
           <div>
-            <label htmlFor="source" className="block mb-1 font-bold">How you get to know us</label>
+            <label htmlFor="source" className="block mb-1 font-bold text-base md:text-lg">How you get to know us</label>
             <input
               type="text"
               id="source"
@@ -132,7 +132,7 @@ export default function Contacts() {
             />
           </div>
           <div>
-            <label htmlFor="questions" className="block mb-1 font-bold">Your concerns and questions</label>
+            <label htmlFor="questions" className="block mb-1 font-bold text-base md:text-lg">Your concerns and questions</label>
             <input
               type="text"
               id="questions"
@@ -144,6 +144,48 @@ export default function Contacts() {
             <Button text="Submit" href='/' style='text-xl font-semibold' />
           </div>
         </form>
+      </div>
+
+      <div className="md:max-w-6xl mx-auto p-4 mb-4 block md:hidden">
+        <h2 className="text-xl md:text-3xl font-bold text-center mb-6">Welcome to visit our offices</h2>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+          <div className="col-span-1 w-40 h-40 md:w-60 md:h-40 text-center">
+            <Image
+              src={"/img/contacts/ny.png"}
+              alt="New York"
+              width={800}
+              height={600}
+            />
+            <span className="font-bold text-secondary mt-3 block">New York, USA</span>
+          </div>
+          <div className="col-span-1 w-40 h-40 md:w-60 md:h-40 text-center">
+            <Image
+              src={"/img/contacts/bs.png"}
+              alt="New York"
+              width={800}
+              height={600}
+            />
+            <span className="font-bold text-primary mt-3 block">Boston, USA</span>
+          </div>
+          <div className="col-span-1 w-40 h-40 md:w-60 md:h-40 text-center">
+            <Image
+              src={"/img/contacts/bj.png"}
+              alt="New York"
+              width={800}
+              height={600}
+            />
+            <span className="font-bold text-secondary mt-3 block">Beijing, China</span>
+          </div>
+          <div className="col-span-1 w-40 h-40 md:w-60 md:h-40 text-center">
+            <Image
+              src={"/img/contacts/gz.png"}
+              alt="New York"
+              width={800}
+              height={600}
+            />
+            <span className="font-bold text-primary mt-3 block">Guangzhou, China</span>
+          </div>
+        </div>
       </div>
 
       <BackButton />
