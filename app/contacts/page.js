195912e734330b105c9515772b795f927fa82ce2
Jacob Sun <jacobsun000@gmail.com>
2024-10-26 03:20:18 -0500

    fix: Contacts page layout

--- app/contacts/page.js
+++ app/contacts/page.js
@@ -14,7 +14,7 @@ const ContactItem = ({ src, alt, text }) => (
       alt={alt}
       className="mr-5 flex-shrink-0"
     />
-    <span className="flex-grow">{text}</span>
+    <span className="flex-grow text-sm md:text-lg">{text}</span>
   </div>
 );
 
@@ -22,51 +22,10 @@ export default function Contacts() {
   return (
     <div>
       <Header currentPath="/contacts" />
-      <div className="md:max-w-6xl mx-auto p-4 mb-4">
-        <SectionHeader title="Our Offices" style='mt-0 mb-8 md:my-8' />
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-          <div className="col-span-1 w-40 h-40 md:w-60 md:h-40 text-center">
-            <Image
-              src={"/img/contacts/ny.png"}
-              alt="New York"
-              width={800}
-              height={600}
-            />
-            <span className="font-bold text-secondary mt-3 block">New York, USA</span>
-          </div>
-          <div className="col-span-1 w-40 h-40 md:w-60 md:h-40 text-center">
-            <Image
-              src={"/img/contacts/bs.png"}
-              alt="New York"
-              width={800}
-              height={600}
-            />
-            <span className="font-bold text-primary mt-3 block">Boston, USA</span>
-          </div>
-          <div className="col-span-1 w-40 h-40 md:w-60 md:h-40 text-center">
-            <Image
-              src={"/img/contacts/bj.png"}
-              alt="New York"
-              width={800}
-              height={600}
-            />
-            <span className="font-bold text-secondary mt-3 block">Beijing, China</span>
-          </div>
-          <div className="col-span-1 w-40 h-40 md:w-60 md:h-40 text-center">
-            <Image
-              src={"/img/contacts/gz.png"}
-              alt="New York"
-              width={800}
-              height={600}
-            />
-            <span className="font-bold text-primary mt-3 block">Guangzhou, China</span>
-          </div>
-        </div>
-      </div>
 
       <div className="py-6">
         <SectionHeader title="Contact Us" style='mt-0 mb-8 md:my-8' />
-        <div className="max-w-4xl mx-auto p-4">
+        <div className="p-4 mx-[8vw]">
           <div className="flex flex-col md:flex-row justify-between md:space-x-12">
             <div className="md:w-1/2">
               <ContactItem
@@ -101,11 +60,11 @@ export default function Contacts() {
         </div>
       </div>
 
-      <div className="max-w-4xl mx-auto p-4 rounded-lg">
-        <h2 className="text-2xl font-bold text-center mb-6">Fill this form and we will get back to you ASAP</h2>
-        <form className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div className="mx-auto p-4 rounded-lg">
+        <h2 className="text-xl font-bold text-center mb-6">Fill this form and we will get back to you ASAP</h2>
+        <form className="grid grid-cols-1 md:grid-cols-2 gap-4 mx-[8vw]">
           <div>
-            <label htmlFor="name" className="block mb-1 font-bold">Your Name*</label>
+            <label htmlFor="name" className="block mb-1 text-lg">Your Name*</label>
             <input
               type="text"
               id="name"
@@ -114,7 +73,7 @@ export default function Contacts() {
             />
           </div>
           <div>
-            <label htmlFor="contact" className="block mb-1 font-bold">Email/Mobile/Wechat*</label>
+            <label htmlFor="contact" className="block mb-1 text-lg">Email/Mobile/Wechat*</label>
             <input
               type="text"
               id="contact"
@@ -123,7 +82,7 @@ export default function Contacts() {
             />
           </div>
           <div>
-            <label htmlFor="source" className="block mb-1 font-bold">How you get to know us</label>
+            <label htmlFor="source" className="block mb-1 text-lg">How you get to know us</label>
             <input
               type="text"
               id="source"
@@ -132,7 +91,7 @@ export default function Contacts() {
             />
           </div>
           <div>
-            <label htmlFor="questions" className="block mb-1 font-bold">Your concerns and questions</label>
+            <label htmlFor="questions" className="block mb-1 text-lg">Your concerns and questions</label>
             <input
               type="text"
               id="questions"
@@ -144,6 +103,48 @@ export default function Contacts() {
             <Button text="Submit" href='/' style='text-xl font-semibold' />
           </div>
         </form>
+      </div>
+
+      <div className="mx-auto p-4 mb-4">
+        <SectionHeader title="Our Offices" style='mt-0 mb-8 md:my-8' />
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mx-[8vw]">
+          <div className="col-span-1 text-center">
+            <Image
+              src={"/img/contacts/ny.png"}
+              alt="New York"
+              width={800}
+              height={600}
+            />
+            <span className="font-bold text-secondary mt-3 block">New York, USA</span>
+          </div>
+          <div className="col-span-1 text-center">
+            <Image
+              src={"/img/contacts/bs.png"}
+              alt="New York"
+              width={800}
+              height={600}
+            />
+            <span className="font-bold text-primary mt-3 block">Boston, USA</span>
+          </div>
+          <div className="col-span-1 text-center">
+            <Image
+              src={"/img/contacts/bj.png"}
+              alt="New York"
+              width={800}
+              height={600}
+            />
+            <span className="font-bold text-secondary mt-3 block">Beijing, China</span>
+          </div>
+          <div className="col-span-1 text-center">
+            <Image
+              src={"/img/contacts/gz.png"}
+              alt="New York"
+              width={800}
+              height={600}
+            />
+            <span className="font-bold text-primary mt-3 block">Guangzhou, China</span>
+          </div>
+        </div>
       </div>
 
       <BackButton />
