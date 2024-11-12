cc9035a3239ae59beeb522858ddd30222d1ca86e
Jacob Sun <jacobsun000@gmail.com>
2024-11-11 21:35:00 -0600

    fix: Partners page layout

--- app/partners/page.js
+++ app/partners/page.js
@@ -23,7 +23,7 @@ export default function Partners() {
       <Header currentPath="/partners" />
       <SectionHeader title="Our Partners" />
       <div className="max-w-4xl mx-auto p-4">
-        <div className="relative mb-10 hidden md:block">
+        <div className="relative mb-10 md:block">
           <Image
             src="/img/partners/map.svg"
             alt="US Map"
@@ -33,29 +33,29 @@ export default function Partners() {
         </div>
         <div className="flex">
           <div className="flex-col justify-center gap-4 mr-2">
-            <div className="bg-white rounded-lg border-2 p-4 w-full mb-2">
+            <div className="bg-white rounded-lg border-2 p-2 md:p-4 w-full mb-2">
               <ul className="text-sm list-disc list-inside">
                 <li>Culver Academies (IN)</li>
               </ul>
             </div>
 
             {/* Second Group */}
-            <div className="bg-white rounded-lg border-2 p-4 w-full">
+            <div className="bg-white rounded-lg border-2 p-2 md:p-4 w-full">
               <ul className="space-y-2 text-sm list-disc list-inside">
-                <li>Episcopal High School(VA)</li>
-                <li>Christchurch School(VA)</li>
+                <li>Episcopal High School (VA)</li>
+                <li>Christchurch School (VA)</li>
               </ul>
             </div>
           </div>
 
           {/* Third Group */}
-          <div className="bg-white rounded-lg border-2 p-4 border-2 w-60 justify-center">
+          <div className="bg-white rounded-lg p-2 md:p-4 border-2 justify-center">
             <ul className="space-y-2 text-sm list-disc list-inside">
-              <li>The Webb Schools(CA)</li>
+              <li>The Webb Schools (CA)</li>
               <li>Stevenson School (CA)</li>
-              <li>Thatcher School(CA)</li>
-              <li>Cate School(CA)</li>
-              <li>Santa Catalina(CA)</li>
+              <li>Thatcher School (CA)</li>
+              <li>Cate School (CA)</li>
+              <li>Santa Catalina (CA)</li>
             </ul>
           </div>
         </div>
@@ -89,8 +89,8 @@ export default function Partners() {
         />
       </div>
 
+      <h2 className="text-2xl md:text-2xl font-bold text-center mb-6">Pictures of past events and admissions</h2>
       <div className="mb-4">
-        <p className="text-center mb-2">Pictures of past events and admissions</p>
         <div className="max-w-7xl mx-auto p-4 grid grid-cols-4 gap-4">
           {[1, 2, 3, 4].map((num) => (
             <div key={num} className="aspect-square">
