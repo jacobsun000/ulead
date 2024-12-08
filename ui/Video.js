6cdf7701a173ecf3aabfc81cf30b481b41bd7691
Jacob Sun <jacobsun000@gmail.com>
2024-12-08 17:47:53 -0600

    fix: Video rounded margin
    fix

--- ui/Video.js
+++ ui/Video.js
@@ -1,6 +1,7 @@
+
 export default function Video({ src, poster }) {
   return (
-    <video className="m-0 p-0" width="640" height="480" controls poster={poster} preload="metadata">
+    <video className="m-0 p-0 block rounded-xl" width="640" height="360" controls poster={poster} preload="metadata">
       <source src={src} type="video/mp4" />
     </video>
   );
