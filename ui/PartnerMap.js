de9fc7196b63da041dba49703db1dd8ae888585f
Jacob Sun <jacobsun000@gmail.com>
2025-02-18 19:53:21 -0600

    feat: PartnerMap color change

--- ui/PartnerMap.js
+++ ui/PartnerMap.js
@@ -68,7 +68,7 @@ export default function PartnerMap({ schools }) {
             {({ geographies }) =>
               geographies.map(geo => {
                 const stateCode = getStateCodeByStateName(geo.properties.name);
-                const fill = regionSchools[index].states.some(s => s.state === stateCode) ? "#DCA5AA" : "#E9CDCF";
+                const fill = regionSchools[index].states.some(s => s.state === stateCode) ? "#f05751" : "#F59793";
                 return (
                   <Geography
                     key={geo.rsmKey}
