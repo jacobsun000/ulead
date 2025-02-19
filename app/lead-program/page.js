d3614eeae536917c0024c87399fae5f22867dd77
Jacob Sun <jacobsun000@gmail.com>
2025-02-18 20:22:27 -0600

    feat: Lead program services image width

--- app/lead-program/page.js
+++ app/lead-program/page.js
@@ -212,7 +212,7 @@ export default function Partners() {
       </div>
 
       <div className="mx-[8vw] mb-8 hidden lg:flex items-center justify-center">
-        <Image src="/img/lead-program/services_desktop.svg" className="max-w-screen-sm" alt="Skills Development" width={1212} height={887} />
+        <Image src="/img/lead-program/services_desktop.svg" className="w-full max-w-6xl" alt="Skills Development" width={1212} height={887} />
       </div>
 
 
