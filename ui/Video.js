e5cec6e8b750dfb1b949cd095f6de72909e565eb
Jacob Sun <jacobsun000@gmail.com>
2025-02-18 21:24:18 -0600

    feat: Disable video download

--- ui/Video.js
+++ ui/Video.js
@@ -1,7 +1,7 @@
 
 export default function Video({ src, poster }) {
   return (
-    <video className="m-0 p-0 block rounded-xl" width="640" height="360" controls poster={poster} preload="metadata">
+    <video className="m-0 p-0 block rounded-xl" width="640" height="360" controls controlsList="nodownload" poster={poster} preload="metadata">
       <source src={src} type="video/mp4" />
     </video>
   );
