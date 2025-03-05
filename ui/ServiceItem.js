76fe7ecded4fce34f85dc11d0b26499bdefd015b
Jacob Sun <jacobsun000@gmail.com>
2025-03-04 21:19:14 -0600

    fix: Service item icon fix

--- ui/ServiceItem.js
+++ ui/ServiceItem.js
@@ -3,8 +3,8 @@ import Image from 'next/image';
 export default function ServiceItem({ title, icon }) {
   return (
     <div className={`border-b py-4 flex gap-4 flex-row`}>
-      <div className="lg:w-8 lg:h-8">
-        <Image src={icon} alt={title} width={40} height={40} />
+      <div className="relative lg:w-8 lg:h-8 w-6 h-6 flex items-center justify-center">
+        <Image src={icon} alt={title} className="object-cover" fill />
       </div>
       <span className="font-semibold text-left">{title}</span>
     </div>
