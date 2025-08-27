d6384dcd2b06739ec8f21a24fcf7cc10ab9b1929
Jacob Sun <jacobsun000@gmail.com>
2025-08-26 23:32:32 -0500

    fix(zh): Default routes for lead-program and summer-school

---
+++ app/zh/lead-program/page.js
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation'
+
+export default function LeadProgramPage() {
+  redirect('/zh/lead-program/highschool')
+}
---
+++ app/zh/summer-school/page.js
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation'
+
+export default function SummerSchoolPage() {
+  redirect('/zh/summer-school/highschool')
+}
